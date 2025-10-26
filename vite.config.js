deadb7d63c4c13a39eaa20afc7ee6e47b7005882
aavrcworld <aavrcworld@gmail.com>
2025-10-26 18:49:41 +0530

    fgdf

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // keep '/' if using a custom domain; use '/aavrc/' if using GitHub Pages URL without a custom domain
+  base: '/aavrc/', // 👈 must match your repo name exactly
 })
