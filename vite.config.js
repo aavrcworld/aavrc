2c9b60f5253cd7ab305b6eb1ddd5516b3b03ace9
aavrcworld <aavrcworld@gmail.com>
2025-10-27 09:20:45 +0530

    fgdfg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aavrc/', // 👈 MUST match your repository name exactly
+  base: '/', // ✅ for custom domain root!
 })
