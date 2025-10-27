b01758047c9e656a2e26c3000a96483680941302
aavrcworld <aavrcworld@gmail.com>
2025-10-27 09:50:16 +0530

    jh

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/aavrc/',
   plugins: [react()],
-});
+  base: '/aavrc/', // 👈 must match your repo name
+  build: {
+    outDir: 'docs' // 👈 ensures build goes directly into docs folder
+  }
+})
